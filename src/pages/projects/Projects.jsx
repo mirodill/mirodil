dbda098c3780bc1ef11a148ae93f492993b62e60
Mirodil <mirodil.mavlonov.2022@gmail.com>
2026-01-25 18:39:15 +0500

    lastcom

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -4,7 +4,14 @@ import ProjectItem from "./ProjectItem";
 import { CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Linkedin, Instagram, Github, Facebook } from "lucide-react";
+import { FaKaggle } from "react-icons/fa";
+import { SiPandas, SiPython, SiPostgresql, SiReact, SiRedux, SiTailwindcss } from "react-icons/si";
+
 import project1 from "../../assets/images/project1.png";
+import project2 from "../../assets/images/transactions.png";
+import project3 from "../../assets/images/shop.png";
+
+
 
 const PROJECTS = [
   {
@@ -13,22 +20,38 @@ const PROJECTS = [
     description:
       "Analyzed loan datasets using Python to assess credit risk, identify high-risk borrowers, and generate actionable insights for financial decision-making.",
     image: project1,
-    link: "https://github.com/username/loan-data",
+    link: "https://github.com/mirodill/loan_data_risk_analysis",
     technologies: [
-      { icon: Github, label: "GitHub" },
-      { icon: Linkedin, label: "LinkedIn" },
+      { icon: SiPython, label: "Python" },
+      { icon: SiPandas, label: "Pandas" },
+      { icon: FaKaggle, label: "Kaggle" },
+
     ],
   },
   {
     id: 2,
-    title: "Personal Portfolio Website",
+    title: "Transaction Analysis",
     description:
-      "React, Tailwind CSS va ShadCN UI yordamida yaratilgan zamonaviy va responsive portfolio web sayt.",
-    image: project1,
-    link: "https://yourportfolio.com",
+      "Analyzed customer transactions using a Kaggle dataset; performed data cleaning, visualization, statistical analysis, and derived actionable insights.",
+    image: project2,
+    link: "https://github.com/mirodill/transaction-analysis",
     technologies: [
-      { icon: Instagram, label: "Instagram" },
-      { icon: Facebook, label: "Facebook" },
+      { icon: SiPython, label: "Python" },
+      { icon: SiPandas, label: "Pandas" },
+      { icon: SiPostgresql, label: "PostgreSQL" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Shop-Line E-commerce",
+    description:
+      "Developed a responsive e-commerce web application using React, Redux, and Tailwind CSS. Implemented client-side routing with React Router, state management with Redux, and API integration using Axios. Enhanced user experience with React Toastify for notifications, Swiper for product carousels, and React Icons for intuitive UI elements. Unique product IDs were handled with UUID to ensure seamless cart and checkout functionality.",
+    image: project3,
+    link: "https://shopline-uz.netlify.app/",
+    technologies: [
+        { icon: SiReact, label: "React" },
+      { icon: SiRedux, label: "Redux" },
+      { icon: SiTailwindcss, label: "Tailwindcss" },
     ],
   },
 ];
